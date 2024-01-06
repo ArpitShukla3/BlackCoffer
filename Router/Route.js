6e50705a859681b0e636e58f13cee51c8884a8e9
ArpitShukla3 <arpit900shukla@gmail.com>
2024-01-06 17:09:05 +0530

    updated

--- Router/Route.js
+++ Router/Route.js
@@ -1,16 +1,11 @@
 import express from "express";
 const route = express.Router();
-import { getUserbyID, oneUserDetail, userCreation, updateUser, filter, querySearch, distinctType } from "../Controller/Controller.js"
+import {querySearch, distinctType } from "../Controller/Controller.js"
 route.get('/', (req, res) => {
     return res.status(200).json({
         message: "good to see you"
     })
 })
-// route.post("/getUserByID", getUserbyID)
-// route.post(`/createUser`, userCreation)
-// route.get(`/filter`, filter)
-// route.post(`/updateUser/:id`, updateUser)
-// route.get(`/:id`, oneUserDetail)
 route.get("/query", querySearch)
 route.get("/search", distinctType)
 
